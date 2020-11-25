703d79f3dcf1b3354e080871535d0e9bff060cac
Redion Kadilliu <rkadilliu@gmail.com>
2020-11-25 12:16:41 -0500

    update

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,19 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
-      <h2>
-        <Link href="/">
-          <a>Back to Home</a>
-        </Link>
-      </h2>
-    </>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. In, ut odit,
+        modi nam quae accusamus enim nesciunt cum at facilis provident eos
+        aliquid fugiat mollitia sapiente non? Placeat, nesciunt eligendi!
+      </p>
+    </Layout>
   );
 }
